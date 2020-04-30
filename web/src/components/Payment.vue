d51c3b8294b9281def9aa6672132f37a7d95a40d
Matheus <mathues@email.com>
2020-04-29 23:11:07 -0300

    feat: passes base system from index.html to web environment

---
+++ web/src/components/Payment.vue
@@ -0,0 +1,110 @@
+<template>
+  <b-row>
+    <b-col cols="3" class="text-right"
+      :style="{
+        textDecoration: payment.suspended ? 'line-through' : null,
+      }"
+    >
+      {{ calculateFinalValue(payment) }}
+    </b-col>
+
+    <div class="col">
+      <span
+        v-for="type in [{
+          name: 'Received on',
+          attribute: 'receivedOn',
+          color: 'info',
+        }, {
+          name: 'Payed on',
+          attribute: 'payedOn',
+          color: 'success',
+        }]"
+        :key="type.attribute"
+        :class="[
+          `badge badge-${type.color} mr-1`,
+          {
+            'd-none': !payment[type.attribute],
+          },
+        ]"
+      >
+        {{ type.name }}
+
+        <span>{{ payment[type.attribute] }}</span>
+      </span>
+
+      <tags :tags="payment.tags"></tags>
+
+      <span
+        :style="{
+          textDecoration: payment.suspended ? 'line-through' : null,
+        }"
+      >
+        {{ upperCaseFirstLetter(payment.description) }}
+      </span>
+
+      <payment v-for="(subPayment, subPaymentIndex) in payment.children" :key="subPaymentIndex" :payment="subPayment"></payment>
+    </div>
+  </b-row>
+</template>
+
+<script>
+
+  import Tags from "./Tags.vue";
+  export default {
+    name: 'payment',
+
+    props: ['payment'],
+
+    components: {
+      Tags,
+    },
+
+    data() {
+      return {};
+    },
+
+    methods: {
+      endEdit(payment) {
+        payment.isEditing = false;
+
+        this.formatAndSave();
+      },
+
+      edit(block, paymentIndex, attribute, subIndex) {
+        if (this.unsavedInput) {
+          return false;
+        }
+
+        let complement = '';
+
+        if (attribute === 'tags') {
+          complement = `-${subIndex}`;
+
+          this.tagInput = block.payments[paymentIndex].tags[subIndex];
+        }
+
+        block.payments[paymentIndex].isEditing = attribute + complement;
+
+        this.$nextTick(() => {
+          this.$refs[`${block.month}-${block.year}-${paymentIndex}-${attribute}${complement}`][0].focus();
+        });
+      },
+
+      calculateFinalValue(payment) {
+        const children = payment.children ? payment.children.map(payment => payment.value) : [];
+
+        let discount = 0;
+
+        if (children.length) {
+          discount = children.reduce((value, current) => value + current);
+        }
+
+        return Number((payment.value - discount).toFixed(2));
+      },
+
+      upperCaseFirstLetter(text) {
+        return text.slice(0, 1).toUpperCase() + text.slice(1);
+      },
+    },
+  }
+</script>
---
+++ web/src/components/Tags.vue
@@ -0,0 +1,18 @@
+<template>
+  <span>
+    <span
+      v-for="(tag, index) in tags"
+      :key="index"
+      class="badge badge-primary mr-1"
+    >
+      {{ tag }}
+    </span>
+  </span>
+</template>
+
+<script>
+  export default {
+    name: 'tags',
+    props: ['tags'],
+  }
+</script>
